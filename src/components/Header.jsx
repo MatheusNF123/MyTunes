a6a146bcf3b445c61dbe53fb13a99f24d6fe2e01
Matheus <matheus_cs.nf@hotmail.com>
2022-05-18 19:21:22 -0300

    exercicio 5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Carregando from '../pages/Carregando';
 import { getUser } from '../services/userAPI';
 
@@ -23,6 +24,11 @@ class Header extends React.Component {
     return (
       <header data-testid="header-component">
         <h1>Header</h1>
+        <nav>
+          <Link data-testid="link-to-search" to="/search"> Pesquisa </Link>
+          <Link data-testid="link-to-favorites" to="/favorites"> favorites </Link>
+          <Link data-testid="link-to-profile" to="/profile"> profile </Link>
+        </nav>
         {carregando
           ? <Carregando /> : <h1 data-testid="header-user-name">{nomeUsuario}</h1>}
       </header>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,12 +2,54 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    nomeB: '',
+    desabilitaBotao: true,
+
+  }
+
+  onInputChange = ({ target }) => {
+    this.setState({ nomeB: target.value }, this.atualizarMudança);
+  }
+
+  atualizarMudança = () => {
+    const numeroMin = 2;
+    const { nomeB } = this.state;
+    if (nomeB.length >= numeroMin) {
+      this.setState({ desabilitaBotao: false });
+    } else {
+      this.setState({ desabilitaBotao: true });
+    }
+  }
+
   render() {
+    const { desabilitaBotao, nomeB } = this.state;
     return (
       <>
         <Header />
         <div data-testid="page-search">
-          <h1>Search</h1>
+          <form action="">
+            <label htmlFor="nome">
+              Nome:
+              <input
+                type="text"
+                value={ nomeB }
+                name="nome"
+                id="nome"
+                data-testid="search-artist-input"
+                onChange={ this.onInputChange }
+              />
+            </label>
+            <button
+              type="button"
+              disabled={ desabilitaBotao }
+              data-testid="search-artist-button"
+              // onClick={ this.salvarNOme }
+            >
+              Pesquisar
+
+            </button>
+          </form>
         </div>
       </>
     );
