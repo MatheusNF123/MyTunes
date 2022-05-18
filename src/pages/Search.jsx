4d9a781997f4050c8ba7c0f7aa5ee167cee457c9
Matheus <matheus_cs.nf@hotmail.com>
2022-05-18 15:20:01 -0300

    exercicio1

---
+++ src/pages/Search.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-search"> algma coisa</div>
+    );
+  }
+}
+export default Search;
